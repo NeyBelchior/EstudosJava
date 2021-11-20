be1cad45bfd9ad77abaa8b6ebbe960e5e80b6d49
Ney Belchior <neybelchior@nomus.com.br>
2021-11-20 20:58:55 -0300

    tipos primitivos teoria

---
+++ TiposPrimitivos.java
@@ -0,0 +1,35 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+	
+	
+	//Temos 8 tipos primitivos em java
+	//Sendo 6 do tipo númericos e o outros  2 dferentes de números
+
+	/* 4 deles representam valores inteiros
+	 * Byte 1  hit -128 +127
+	 * Short 2 bytes hit +- 32767
+	 * int 4 bytes
+	 * long 8bytes
+	 * >>A diferença entre eles é basicamente a capacidade de armazenamento
+	 * 1 byte = 8bits
+	 * +0101100
+	 * Pontos flutuantes
+	 * Float 4 byte
+	 * double 8 bytes
+	 * 
+	 * -------
+	 * Char>um caracter delimitado por aspas simples '' numero '1' é um char e nao um valor inteiro
+	 * Bolean > sempre será false e true
+	 * 
+	 * Double a = 3; Qualquer literal inteiro vai ser um int <UM TIPO MENOR QUE FOI CONVERTIDO PARA UM TIPO MAIOR (INT>DOUBLE)
+	 * Double b = 3,2; Qualquer literal do tipo flutuante sera double
+	 *
+	 * Sempre que você sai de um tipo que tem capcidade menor de armazenamento e vai para um que tem maior
+	 * o java vai entender que tudo que está dentro desse tipo vai caber dentro desse outro tipo e não vai ter problema
+	 * no contrário do maior para um menor você precisa ser mais explícito se não pode gerar problema
+	 * 
+	 * Tocar os tipos de dados não geram uma otimização relevante pelo trabalho e pode trazer outros problemas  a não ser que sejam muito e muitos dados
+	 */
+	
+}
