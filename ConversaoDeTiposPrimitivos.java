1ba2b192eb5c78ee90529e7ebe0debe768d78bed
Ney Belchior <neybelchior@nomus.com.br>
2021-11-21 20:02:59 -0300

    ConversaoDeTiposPrimitivos

---
+++ ConversaoDeTiposPrimitivos.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class ConversaoDeTiposPrimitivos {
+	
+	/* byte, short, int , long
+	 * covertendo >> não tem perigo de perder informação  ao <<< sim pode causar perda de informação 
+	 * O java converte de forma implicita o tipo menor para outro maior pois não haverá problema (ele concerteza vai caber)
+	 * agora  ao contrário a conversão deve ser feita de forma explícita
+	 * 
+	 * 
+	 * inteiro para um float ok ao contrario perde casas decimais
+	 * 
+	 * O java não olha valor , ele olha tipo
+	 * Se você tem uma variável de valor 1 do tipo long msmo que o 1 caiba nos niveis anteriores a conversão de terá de ser explícita(cast) 
+	 * 
+	 *
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+
+}
