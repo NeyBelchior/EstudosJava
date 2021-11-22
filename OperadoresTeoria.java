a237088b35c680a0ed77c0f8fccbfccd7eca555a
Ney Belchior <neybelchior@nomus.com.br>
2021-11-21 21:45:36 -0300

    OperadoresTeoria

---
+++ OperadoresTeoria.java
@@ -0,0 +1,46 @@
+package fundamentos;
+
+public class OperadoresTeoria {
+	
+	
+	/* Operadores :
+	 * 
+	 * Unários / binários / ternário 
+	 * 
+	 * Essa classificação tem relação com o número de OPERANDOS em cima de determinado operador
+	 * 
+	 * Exemplo : Uma soma é um operador binario ,pois
+	 * você tem o operador "+" e 2 operandos , por exemplo 2+3
+	 * 
+	 * unário 
+	 * a++ incremento >Temos aqui um unico operando (a) em cima de um operador de incremento (++)
+	 * 
+	 * ternáro
+	 * 
+	 * v?v:v 2 operadores e 3 operandos
+	 * 
+	 * Tipos de operadores:
+	 * 
+	 *-Aritiméticos
+	 *-Relacionais
+	 *-lógicos
+	 *-Atribuição 
+	 *
+	 *Prefix , posfi infix
+	 *
+	 *++a    , a++,   3+2
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+}
