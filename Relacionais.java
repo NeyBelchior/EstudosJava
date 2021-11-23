4944ba15018aae981a0ee7709cdd45dcba5a1059
Ney Belchior <neybelchior@nomus.com.br>
2021-11-22 22:07:24 -0300

    Operadores relacionais

---
+++ Relacionais.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class Relacionais {
+
+	public static void main(String[] args) {
+		
+		int a = 97;
+		int b = 'a'; // cada letra tem associado consigo uma posição na tabela unicode 
+		
+		System.out.println(a == b);
+		
+		System.out.println('\u0061'+" 97 na base hexadecimal");
+		
+		System.out.println(3 > 4);
+		System.out.println(3>=3);
+		System.out.println(3< 7);
+		System.out.println(30 <= 7);
+		System.out.println(30 !=70);
+		
+		
+		
+		
+				
+
+	}
+
+}
